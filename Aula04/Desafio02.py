94ba3dc01f46fef399eb367733954e4b0dca73dc
Arthur Cruz <73409867+ArtCruz@users.noreply.github.com>
2022-07-19 21:37:08 -0300

    Add files via upload

---
+++ Aula04/Desafio02.py
@@ -0,0 +1,4 @@
+Dia = input('Que dia você nasceu?')
+Mes = input('Que mês você nasceu?')
+Ano = input('Que ano você nasceu?')
+print('Você nasceu dia', Dia, 'de', Mes, 'de', Ano,'. Correto?')
---
+++ Aula04/Desafio03.py
@@ -0,0 +1,4 @@
+var1 = int ( input('Escolha um número: '))
+var2 = int ( input('Escolha outro número: '))
+soma = var1 + var2
+print('A soma desses números é', soma)             
